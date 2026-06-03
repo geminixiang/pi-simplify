aaed792d4ad03fc000dbb86fe6df80795ca2fa42
Ying Xiang <f416720001@yahoo.com.tw>
2026-06-03 23:59:50 +0800

    refactor: remove code-smell skill and skill path registration

--- index.ts
+++ index.ts
@@ -6,19 +6,10 @@
  */
 
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { registerCodeSmellWorkflow } from "./src/code-smell/workflow.js";
 import { registerSimplifyWorkflow } from "./src/simplify/workflow.js";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default function simplifyExtension(pi: ExtensionAPI) {
-  pi.on("resources_discover", () => ({
-    skillPaths: [path.join(__dirname, "skills")],
-  }));
-
   registerSimplifyWorkflow(pi);
   registerCodeSmellWorkflow(pi);
 }
