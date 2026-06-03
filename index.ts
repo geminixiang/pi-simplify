5cef96ebd7f9558bc0b2e5405bcd0e1f830c98ec
Ying Xiang <f416720001@yahoo.com.tw>
2026-06-03 23:56:07 +0800

    refactor: move source files into src/

--- index.ts
+++ index.ts
@@ -9,8 +9,8 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { registerCodeSmellWorkflow } from "./code-smell-workflow.js";
-import { registerSimplifyWorkflow } from "./workflow.js";
+import { registerCodeSmellWorkflow } from "./src/code-smell-workflow.js";
+import { registerSimplifyWorkflow } from "./src/workflow.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
